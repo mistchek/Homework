92d597f67760e6362facb2e052a2fb0568336a26
mistchek <zadoiannaia@gmail.com>
2025-11-05 18:22:50 +0300

    fix3

--- conv.py
+++ conv.py
@@ -1,45 +1,33 @@
 # Homework
 # Задоянная Анастасия, Дубина Александра БИ-252
-import requests
-from bs4 import BeautifulSoup
+from get_rate import getrates
 
-
-def getrates():
-    try:
-        url = 'https://cbr.ru/currency_base/daily/'
-        response = requests.get(url)
-
-
-        soup = BeautifulSoup(response.content, 'html.parser')
-        rates = {'RUB': 1.0}
-
-        # Ищем в тексте таблицу с курсами валют
-        currencies = ['USD', 'EUR', 'GBP', 'JPY', 'CNY', 'CHF', 'CAD', 'AUD']
-
-        table = soup.find('table', class_='data')
-        if table:
-            rows = table.find_all('tr')
-            for row in rows[1:]:
-                cols = row.find_all('td')
-                if len(cols) >= 5:
-                    currcode = cols[1].text.strip() #извлекаем код валюты
-                    value = cols[4].text.strip().replace(',', '.') #преобразовываем число в нужный тип для питона
-                    # Добавляем только если валюта в нашем списке
-                    if currcode in currencies and value:
-                        try:
-                            rates[currcode] = float(value) #текст в число
-                        except ValueError:
-                            continue
-
-
-        return rates
-    except Exception as e:
-        print(f'ERROR: {e}')
-        return None
-
-
-if __name__ == '__main__':
+def main():
     rates = getrates()
-    if rates:
-        for curr, rate in rates.items():
-            pass
+    if not rates:
+        print('ERROR: No rates found')
+        return
+    while True:
+        print(f"CURRENCIES: {','.join(rates.keys())}")
+        try:
+            summa=float(input('Enter summa: '))
+            fromcur=(input('Enter from: ').upper())
+            tocur=(input('Enter to: ').upper())
+            if fromcur not in rates:
+                print(f'ERROR: {fromcur} not in rates')
+                continue
+            if tocur not in rates:
+                print(f'ERROR: {tocur} not in rates')
+                continue
+            res=summa*rates[fromcur]/rates[tocur]
+            print(f'RES: {summa}{fromcur} = {res}{tocur}')
+        except ValueError:
+            print(f'ERROR: try number')
+            continue
+
+        con=input('Do you want to continue (y/n)? ').lower()
+        if con not in ['y','yes','да']:
+            print('SEE YOU LATER!')
+            break
+if __name__ == '__main__':
+    main()
